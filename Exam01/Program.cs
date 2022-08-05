3b0d4ae35ca473d42bbbe8213b813ba8153a54bb
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-06 00:47:25 +0200

    fix

--- Exam01/Program.cs
+++ Exam01/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите первое число ");
 int numberA = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int numberB = int.Parse(Console.ReadLine());
-if (numberA == numberB){Console.WriteLine("Эти числа одинаковые");}
-else if (numberA > numberB){Console.Write("Первое число " + (numberA ));Console.WriteLine(" больше, чем второе число " + (numberB));}
-else{Console.Write("Второе число " + (numberB));Console.WriteLine(" больше, чем первое число " + (numberA));}
+if (numberA == numberB) { Console.WriteLine("Эти числа одинаковые"); }
+else if (numberA > numberB) { Console.Write("Первое число " + (numberA)); Console.WriteLine(" больше, чем второе число " + (numberB)); }
+else { Console.Write("Второе число " + (numberB)); Console.WriteLine(" больше, чем первое число " + (numberA)); }
     
