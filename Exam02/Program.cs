c31222a7eaa417883cae1ff99114c96e229a4838
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-06 00:41:01 +0200

    exam done

--- Exam02/Program.cs
+++ Exam02/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Введите первое число ");
 int numberC = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите третье число ");
 int max = numberA;
-if(numberA == numberB && numberB == numberC){Console.WriteLine("Эти числа одинаковые и максимальное число " + max);}
-if(numberB > max){max = numberB;}
-if (numberC > max){max = numberC;}
+if (numberA == numberB && numberB == numberC) { Console.WriteLine("Эти числа одинаковые и максимальное число " + max); }
+if (numberB > max) { max = numberB; }
+if (numberC > max) { max = numberC; }
 Console.WriteLine("Максимальное число " + (max));
