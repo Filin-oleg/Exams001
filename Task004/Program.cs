e597d814ce6af561bf0dfd0f4604a2f66a939f67
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-06 11:47:50 +0200

    Program done

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,1 +1,7 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до введенного Вами числа");
+for (int a = 2; a <= number; a ++)
+{if (a % 2 == 0){Console.WriteLine(a);}}
